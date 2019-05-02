ca81fa3f41e8fea8b79cd0c0e38699fddc75ad89
morp <rutesicm27@gmail.com>
2019-05-02 02:41:55 +0200

    Add grid

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -1,20 +1,50 @@
 class Board
-  def draw_board
-    puts "========================="
-    puts "-------------------------"
-    puts "|\t|\t|\t|"
-    puts "-------------------------"
-    puts "|\t|\t|\t|"
-    puts "-------------------------"
-    puts "|\t|\t|\t|"
-    puts "-------------------------"
-    puts "========================="
+  # def self.draw_board
+  #   puts "========================="
+  #   puts "-------------------------"
+  #   puts "|\t|\t|\t|"
+  #   puts "-------------------------"
+  #   puts "|\t|\t|\t|"
+  #   puts "-------------------------"
+  #   puts "|\t|\t|\t|"
+  #   puts "-------------------------"
+  #   puts "========================="
+  # end
+
+  attr_reader :grid
+
+  def initialize(input = {})
+    @grid = input.fetch(:grid, default_grid)
   end
 
+  def get_cell(x, y)
+    grid[y][x]
+  end
+
+  private
+  def default_grid
+    Array.new(3) {Array.new(3) {" "}}
+  end
 end
 
 class Cell 
-  attr_accessor :options
-  options = {x: "X", o: "O", empty: ""}
+  def initialize(value = "")
+    @value = value
+  end
+end
 
+class Game
+  def is_draw?
+    @turn == 9
+  end
+end
+
+class Player
+  attr_reader :name
+  attr_reader :marker
+
+  def initialize(input)
+    @name = input.fetch(:name)
+    @marker = inut.fetch(:marker)
+  end
 end
