85485241b469437f6207525ae1c651800d3c3022
morp <rutesicm27@gmail.com>
2019-05-05 17:57:26 +0200

    Pseudo Code

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -1,50 +1,23 @@
-class Board
-  # def self.draw_board
-  #   puts "========================="
-  #   puts "-------------------------"
-  #   puts "|\t|\t|\t|"
-  #   puts "-------------------------"
-  #   puts "|\t|\t|\t|"
-  #   puts "-------------------------"
-  #   puts "|\t|\t|\t|"
-  #   puts "-------------------------"
-  #   puts "========================="
-  # end
-
-  attr_reader :grid
-
-  def initialize(input = {})
-    @grid = input.fetch(:grid, default_grid)
-  end
-
-  def get_cell(x, y)
-    grid[y][x]
-  end
-
-  private
-  def default_grid
-    Array.new(3) {Array.new(3) {" "}}
-  end
-end
-
-class Cell 
-  def initialize(value = "")
-    @value = value
-  end
-end
-
 class Game
-  def is_draw?
-    @turn == 9
-  end
+  # init
+    # set up board
+    # set up players
+    
+  # play
+
+
 end
 
 class Player
-  attr_reader :name
-  attr_reader :marker
+  # init
+    # set name and token
+end
 
-  def initialize(input)
-    @name = input.fetch(:name)
-    @marker = inut.fetch(:marker)
-  end
+class Board
+  # init
+    #set up blank board
+
+  # render
+    # display board with existing tokens if any
+
 end
