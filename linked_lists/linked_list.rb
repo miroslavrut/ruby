ce25c2edfb6ebbb05fb8b510749857ace3677228
morp <rutesicm27@gmail.com>
2019-07-18 19:34:55 +0200

    Initial commit

---
+++ linked_lists/linked_list.rb
@@ -0,0 +1,33 @@
+class LinkedList
+  #append
+
+  #prepend
+
+  #size
+
+  #head
+
+  #tail
+
+  #at(index)
+
+  #pop
+
+  #contains?
+
+  #find(data)
+
+  #to_s
+
+  #insert_at(index)
+
+  #remove_at(index)
+end
+
+class Node
+  #value
+  #next_node
+end
+
+
+
