a9ad4586aa033a7900c481cdd8f3146a06591ce8
morp <rutesicm27@gmail.com>
2019-07-30 19:48:36 +0200

    Fix paths

--- tic_tac_toe/lib/game.rb
+++ tic_tac_toe/lib/game.rb
@@ -1,5 +1,5 @@
-require "./board.rb"
-require "./player.rb"
+require "board.rb"
+require "player.rb"
 
 class Game
   attr_accessor :grid
@@ -121,6 +121,6 @@ class Game
   end
 end
 
-Game.new
+# Game.new
 
 
